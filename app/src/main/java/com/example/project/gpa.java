a285ee8ed5343b2209b7722d491c85ce5f738c19
EtafQudsi <etafkodsi@gmail.com>
2022-04-08 16:07:20 +0200

    try

--- app/src/main/java/com/example/project/gpa.java
+++ app/src/main/java/com/example/project/gpa.java
@@ -3,12 +3,19 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class gpa extends AppCompatActivity {
-
+    LinearLayout v;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gpa);
+        v=findViewById(R.id.value);
+    }
+
+    public void calc(View view) {
+        //v.setVisibility(View.VISIBLE);
     }
 }
